6e95ba06186c89e91fcd057372f2c9cee618ba86
Mihai Mangu <mihai.mangu@gmail.com>
2023-07-12 13:56:58 +0300

    update menu

--- src/components/Organisms/AppHeader.jsx
+++ src/components/Organisms/AppHeader.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import logo4x4 from '../../images/4x4-logo.png';
+import './AppHeader.scss';
 
 
 const AppHeader = () => {
@@ -11,6 +12,12 @@ const AppHeader = () => {
                     <Link to="/" className="App-link">
                         <img src={logo4x4} className="App-logo" alt="logo" />
                     </Link>
+                </div>
+                <div className="app-menu__wrapper">
+                    <div className="app-menu__main">
+                        <Link className="app-menu__item">Home</Link>
+                        <Link to="/jobs" className="app-menu__item">Jobs</Link>
+                    </div>
                 </div>
             </header>
         </div>
