d832285b236daa7a0b4dbf12cb781952760a49ec
Mihai Mangu <mihai.mangu@gmail.com>
2023-08-08 22:26:36 +0300

    update admin list

--- src/components/Pages/AdList/AdminList.jsx
+++ src/components/Pages/AdList/AdminList.jsx
@@ -17,13 +17,28 @@ function AdminAdList(props){
     const [currentPage, setCurrentPage] = useState(1);
     const [totalPages, setTotalPages] = useState(15);
     const [filterSettings, setFilterSettings] = useState({})
+    const [hiddenCarsCount, setHiddenCarsCount] = useState(0);
+    const [unscrapedCarsCount, setUnscrapedCarsCount] = useState(0);
+    const [totalCount, setTotalCount] = useState(0);
+
 
     const grabAdsFromApi = (settings = {}) => {
         setLoading(true);
+        settings.includeUnscraped = true;
+        settings.includeHidden = true;
         getList(settings).then((res) => {
             setLoading(false);
             if(res.data?.offroadCars){
                 setCars(res.data.offroadCars);
+            }
+            if(res.data?.hiddenCars){
+                setHiddenCarsCount(res.data.hiddenCars);
+            }
+            if(res.data?.notScrapedCars){
+                setUnscrapedCarsCount(res.data.notScrapedCars);
+            }
+            if(res.data?.totalCount){
+                setTotalCount(res.data.totalCount);
             }
             if(res.data?.totalPages){
                 setTotalPages(res.data.totalPages);
@@ -49,7 +64,7 @@ function AdminAdList(props){
 
         let searchParams = {
             ...filterSettings,
-            page: currentPage
+            page: currentPage,
         }
         grabAdsFromApi(searchParams)
 
@@ -67,6 +82,11 @@ function AdminAdList(props){
             <h1>Administreaza anunturi</h1>
             {isError && <div>error while getting data</div>}
             <AdFilters onSearch={filterAds}  />
+            <div className="cars-list__admin-details">
+                {cars.length && <div className="cars-list__admin-details__item">Total: {totalCount}</div>}
+                <div className="cars-list__admin-details__item">Unscraped: {unscrapedCarsCount}</div>
+                <div className="cars-list__admin-details__item">Hidden: {hiddenCarsCount}</div>
+            </div>
             <div className="cars-list__inner-wrapper">
                 {loading ? <LoadingWrapper /> : cars.length && cars.map((car) => {
                     return <Car key={car._id} data={car} />
